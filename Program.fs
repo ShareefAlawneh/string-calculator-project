1b63d2a48519b1b83dcd9a463986df9832c839cc
shareef alawneh <shareef@b-leaders.com>
2018-09-05 23:39:18 +0300

    Adding The Support For Custom Delimeters in any Manner

--- Program.fs
+++ Program.fs
@@ -18,5 +18,10 @@ let main argv =
    printf "\n\n---- Starting The \\n Delimeter String Test ----\n"
    printf "Input: \"1,2\\n3\"\n"
    printf "Result:\t"
-   let test2 = "1,2\n3" |> StringNormalizer |> SplitToList |> Add  
+   let test3 = "1,2\n3" |> StringNormalizer |> SplitToList |> Add  
+   
+   printf "\n\n---- Starting The Custom Delimeters String Test ----\n"
+   printf "Input: \"//[**][&]\\n1,2\\n3\"\n"
+   printf "Result:\t"
+   let test4 = "//[**][&]\n1,2\n3" |> StringNormalizer |> SplitToList |> Add  
    0 // return an integer exit code
