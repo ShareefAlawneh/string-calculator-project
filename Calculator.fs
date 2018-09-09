2d324c97c142691583b33082126d1eb73c1e59f9
shareef alawneh <shareef@b-leaders.com>
2018-09-09 17:39:37 +0300

    Optimize The Code And Remove The Loop

--- Calculator.fs
+++ Calculator.fs
@@ -11,24 +11,18 @@ module public Calculator =
         let LessThan1000 num = if num < 1000 then true else false
         List.sum (List.filter LessThan1000 nums) |> printf "%A" 
 
-    let SplitToList (stringSeq: string) = 
-       stringSeq.Split [|','|] |>  Array.map System.Int32.Parse |>  Array.toList
+    let SplitToList (stringSeq: string, dels: char []) =
+        let delimeters = Array.concat [|[|','; '\n'|] ; dels|] 
+        stringSeq.Split (delimeters, StringSplitOptions.RemoveEmptyEntries) |>  Array.map System.Int32.Parse |>  Array.toList
 
     let NormalizeStringWithSpecialDelimeters (stringSeq: string) = 
         let delimeterEnd = stringSeq.IndexOf("\n")
         let mutable delimeters = stringSeq.[0..delimeterEnd-1]
         let mutable finalString = stringSeq.Remove(0, delimeterEnd+1).Replace("\n", ",")
-        delimeters <- delimeters.Replace("//","")
-        let mutable delimeter = ""
-        if delimeters.Length = 1
-        then finalString <- finalString.Replace(delimeters, ",")
-        else for c in delimeters do 
-                if c = '[' then
-                   delimeter <- ""
-                elif c = ']' then finalString.Replace(delimeter, ",") |> ignore
-                else delimeter <-  delimeter + string c
-        finalString
+        let splittedDelimeters = delimeters.Split([|'[';']'|],  StringSplitOptions.RemoveEmptyEntries)
+        let delString = splittedDelimeters |> String.concat ""
+        SplitToList(finalString, delString.ToCharArray())
            
     let StringNormalizer (stringSeq: string) = 
-        if stringSeq.Equals("") then "0" 
-        elif stringSeq.StartsWith("//") then  stringSeq |> NormalizeStringWithSpecialDelimeters else stringSeq.Replace("\n",",")
+        if stringSeq.Equals("") then [0] 
+        elif stringSeq.StartsWith("//") then  stringSeq |> NormalizeStringWithSpecialDelimeters else SplitToList (stringSeq, [|' '|])
