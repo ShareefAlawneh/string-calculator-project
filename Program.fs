ac89e11bb6b8cbf3e4c0d2dbd313f8816d85cc8a
shareef alawneh <shareef@b-leaders.com>
2018-09-09 17:42:57 +0300

    Refactor The Inputs

--- Program.fs
+++ Program.fs
@@ -8,31 +8,31 @@ let main argv =
    printf "---- Starting The Empty String Test ----\n"
    printf "Input: \"\"\n"
    printf "Result:\t"
-   let test1 = "" |> StringNormalizer  |> Add
+   "" |> StringNormalizer  |> Add
    
    printf "\n\n---- Starting The Simple String Test ----\n"
    printf "Input: \"1,2\"\n"
    printf "Result:\t"
-   let test2 = "1,2" |> StringNormalizer |> Add 
+   "1,2" |> StringNormalizer |> Add 
 
    printf "\n\n---- Starting The \\n Delimeter String Test ----\n"
    printf "Input: \"1,2\\n3\"\n"
    printf "Result:\t"
-   let test3 = "1,2\n3" |> StringNormalizer |> Add  
+   "1,2\n3" |> StringNormalizer |> Add  
 
    printf "\n\n---- Starting The Custom Delimeters String Test ----\n"
-   printf "Input: \"//[**][&]\\n1,2\\n3\"\n"
+   printf "Input: \"//[$][&]\n1$2&\"\n"
    printf "Result:\t"
-   let test4 = "//[**][&]\n1,2\n3" |> StringNormalizer  |> Add 
+   "//[$][&]\n1$2&3" |> StringNormalizer  |> Add 
    
    printf "\n\n---- Starting The Single Custom Delimeters String Test ----\n"
    printf "Input: \"//;\\n1;2;3;4;5\n6\"\n"
    printf "Result:\t"
-   let test5 = "//;\n1;2;3;4;5\n6" |> StringNormalizer |> Add 
+   "//;\n1;2;3;4;5\n6" |> StringNormalizer |> Add 
 
    printf "\n\n---- Starting The Thousnd String Test ----\n"
-   printf "Input: \"//[**][&]\\n1,2\\n3,10000\"\n"
+   printf "Input: \"//[**][&]\n1**2&3,10000\"\n"
    printf "Result:\t"
-   let test6 = "//[**][&]\n1,2\n3,10000" |> StringNormalizer |> Add 
+   "//[**][&]\n1**2&3,10000" |> StringNormalizer |> Add 
    
    0 // return an integer exit code
